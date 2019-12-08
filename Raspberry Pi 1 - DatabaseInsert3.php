e45d724e239eeced15dfff54d91327fdaf4d6fe1
Pece17 <31437201+Pece17@users.noreply.github.com>
2019-12-08 15:24:27 +0200

    Create Raspberry Pi 1 - DatabaseInsert3.php

---
+++ Raspberry Pi 1 - DatabaseInsert3.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "172.28.175.41";
+$username = "raspbian_1";
+$password = "MonialaProjekti";
+$dbname = "iotbeacon";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO beaconusers (beacon_name, beacon_mac_address, user_first_name, user_last_name, room_name) VALUES ('Beacon3', 'f2:36:00:21:c0:50', 'Niko', 'Kulmanen', 'Servula');";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
